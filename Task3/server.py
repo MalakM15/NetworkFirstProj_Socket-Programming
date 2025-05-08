1d2587554e0d1bf870c77626238eeae4932df3e1
MalakM15 <mjmilhem15@gmail.com>
2025-05-08 23:08:16 +0300

    task3 server file

--- Task3/server.py
+++ Task3/server.py
@@ -13,18 +13,84 @@ GUESS_TIME = 10   # seconds
 GUESS_RANGE = (1, 100)
 #serverName = "game main server"
 serverIP = "127.0.0.1"
-Player_1 = "Milhem_Malak"
-Player_2 = "Nasser_Taymaa"
+player_names = {
+    "Player_1": "Milhem_Malak",
+    "Player_2": "Nasser_Taymaa"
+}
 ##TCP connection:
 TCPPort = 6000
-serverSocket = socket(AF_INET,SOCK_STREAM) #TCP
-
+tcp_server_socket = socket(AF_INET, SOCK_STREAM) #TCP
+tcp_server_socket.bind((serverIP, TCPPort))
+tcp_server_socket.listen()
 ##UDP connection:
 UDPPort = 6001
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP
-server_socket.bind((serverIP, UDPPort))
+udp_server_socket = socket(AF_INET, SOCK_DGRAM) #UDP
+udp_server_socket.bind((serverIP, UDPPort))
 
-generate_random_num
+def generate_random_num():
+    return random.randint(*GUESS_RANGE)
+def handle_client(connection, address):
+    try:
+        connection.send(b"Welcome to the game! Please send: JOIN <your_name>\n")
+        data = connection.recv(1024).decode().strip()
+        if data.startswith("JOIN"):
+            _, name = data.split(" ", 1)
+            if name in active_clients.values():
+                connection.send(b"Name already used. Use another name.\n")
+                connection.close()
+                return
+            active_clients[connection] = name
+            print(f"{name} joined from {address}")
+            connection.send(b"Signed Successfully. Waiting for others...\n")
+        else:
+            connection.send(b"Invalid response. Connection closing.\n")
+            connection.close()
+    except:
+        connection.close()
+def handle_guesses(secret_number):
+    while True:
+        try:
+            udp_data, client_addr = udp_server_socket.recvfrom(1024)
+            guess_msg = udp_data.decode().strip()
+            if not guess_msg.isdigit():
+                udp_server_socket.sendto(b"Invalid guess. Must be a number.\n", client_addr)
+                continue
 
+            guess = int(guess_msg)
+            if guess < GUESS_RANGE[0] or guess > GUESS_RANGE[1]:
+                udp_server_socket.sendto(b"Warning: Out of the range, miss your chance\n", client_addr)
+                continue
 
+            if guess < secret_number:
+                udp_server_socket.sendto(b"Higher\n", client_addr)
+            elif guess > secret_number:
+                udp_server_socket.sendto(b"Lower\n", client_addr)
+            else:
+                udp_server_socket.sendto(b"Correct!\n", client_addr)
+ #               game_result(client_addr)
+                break
+        except:
+            continue
+
+#def game_result():
+
+active_clients = {}
 def start_round():
+    global guess_start_time
+
+    while len (active_clients)< MIN_PLAYERS:
+        print ("Waiting for Players ...\n")
+        time.sleep(5)
+    print("Minimum players reached, get ready.\n ")
+    start_msg = f"Game started with {len(active_clients)} players. \n You have 60 seconds to guess the number between {GUESS_RANGE[0]} and {GUESS_RANGE[1]}!\n".encode()
+    for conn in active_clients:
+        conn.send(start_msg)  #send the start messege for all clients
+
+    secret_number = generate_random_num()
+    print(f"Secret number is {secret_number}")
+    guess_start_time = time.time()
+
+    # Start UDP guessing listen
+    Thread(target=handle_guesses, args=(secret_number,)).start()
+print("Connection established. Waiting for players...")
+Thread(target=start_round).start()    
